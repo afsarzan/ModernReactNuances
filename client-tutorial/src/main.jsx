9927b85bf97cf6069bddf33db5a1f51db79dd1b3
afsarzan <afsar_zan@yahoo.com>
2025-12-26 16:02:45 -0800

    feat: Implement ThemeContext and integrate theme toggling in Header component

--- client-tutorial/src/main.jsx
+++ client-tutorial/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-// ⏸️ WORKSHOP STEP 5: Add Context
-// TODO: Import ThemeProvider from './context/ThemeContext.jsx'
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 import App from "./App.jsx";
 import "./index.css";
@@ -11,8 +9,10 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* ⏸️ WORKSHOP STEP 5: Wrap in ThemeProvider */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
